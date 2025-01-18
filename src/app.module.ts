3218502962af56eb4b846f9feb4705c75f1729b6
Mohaiminul Eraj <eraj471@gmaIl.com>
2025-01-18 18:56:55 +0600

    configService issue fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      cache: true,
+    }),
+    ThrottlerModule.forRootAsync({
+      useFactory: async () => ({
+        throttlers: [
+          {
+            ttl: parseInt(process.env.RATE_LIMITER_TIME_TO_LEAVE, 10) || 60000, // default to 60000 if env variable not present
+            limit: parseInt(process.env.RATE_LIMITER_MAX_TRY, 10) || 60, // default to 60 if env variable not present
+          },
+        ],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('APP_PORT', 5000));
 }
 bootstrap();
